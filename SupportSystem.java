668fdb866083001293fb665740b71dbe400cd2ed
alex1231laurin-byte <alex1231.laurin@gmail.com>
2025-10-06 14:13:21 -0400

    Update SupportSystem.java

--- SupportSystem.java
+++ SupportSystem.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 /**
  * This class implements a technical support system. It is the top level class 
  * in this project. The support system communicates via text input/output 
@@ -37,13 +39,13 @@ public class SupportSystem
         printWelcome();
 
         while(!finished) {
-            String input = reader.getInput().trim().toLowerCase();
+            HashSet<String> input = reader.getInput();
 
-            if(input.startsWith("bye")) {
+            if(input.contains("bye")) {
                 finished = true;
             }
             else {
-                String response = responder.generateResponse();
+                String response = responder.generateResponseFromHashSet(input);
                 System.out.println(response);
             }
         }
